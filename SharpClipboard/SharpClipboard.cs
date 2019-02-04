3383c9293727ef0d713f79c697be9b47d87c5eca
Willy-Kimura <wilskym@live.com>
2019-02-04 11:05:52 +0300

    Add project files.

---
+++ SharpClipboard/SharpClipboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace WK.Libraries.SharpClipboard
+{
+    public partial class SharpClipboard : Component
+    {
+        #region Constructors
+
+        public SharpClipboard()
+        {
+            InitializeComponent();
+        }
+
+        public SharpClipboard(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
