4be2fca40450f438b24eb44003ac17bc899b76a9
Christoph Fink <fink.christoph@gmail.com>
2020-04-09 20:27:57 +0200

    Update test-project to .NET Core 3.0

--- SharpClipboard/Properties/Resources.Designer.cs
+++ SharpClipboard/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SharpClipboard.Properties {
+namespace WK.Libraries.SharpClipboardNS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SharpClipboard.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SharpClipboard.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WK.Libraries.SharpClipboardNS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
